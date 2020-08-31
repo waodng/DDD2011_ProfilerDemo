5ba20bd6ffcfcc0169870b0bf34e8f6df6d24a73
waodng <454802367@qq.com>
2020-08-31 16:35:31 +0800

    modify readme.md

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
+### 说明
+
+该项目是通过CLR公共语言运行时的API来在JIT（即时编译）时期对代码进行IL注入来达到修改程序的目的。
+
+CLR 的Profile api接口在微软官网可找到C++的说明文档，该API调用只能通过C++来调用，无法使用C#来调用
+
+
+
+* 本项目的运行时先把DDDProfile.dll类库通过注册COM组件的方式，然后设置环境变量
+
+~~~
+startInfo.EnvironmentVariables.Add("Cor_Profiler", "{BDD57A0C-D4F7-486D-A8CA-86070DC12FA0}");
+startInfo.EnvironmentVariables.Add("Cor_Enable_Profiling", "1");
+            
+~~~
+
+的方式来运行，每次程序运行时，CLR会自动启动com组件来运行注入代码程序。。。
+
+...
+
 ### Purpose
+
 Demo code for the DDD Melbourne 2011 Profiler Talk
 
 ### Licence
--- step5_main_injected_method_object_array/ProfilerHost/Program.cs
+++ step5_main_injected_method_object_array/ProfilerHost/Program.cs
@@ -18,20 +18,20 @@ namespace ProfilerHost
             var currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             Console.WriteLine("Executing Assembly:\n{0}\n", Assembly.GetExecutingAssembly().Location);
             Console.WriteLine("Current directory:\n{0}\n", currentDirectory);
-            //var argument = "/s " + Path.Combine(currentDirectory, "DDDProfiler.dll");
-            //var registerProfilerDll = Process.Start("regsvr32", argument);
-            //Console.WriteLine("Running:\nregsvr32 {0}\n", argument);
-            //registerProfilerDll.WaitForExit();
+            var argument = "/s " + Path.Combine(currentDirectory, "DDDProfiler.dll");
+            var registerProfilerDll = Process.Start("regsvr32", argument);
+            Console.WriteLine("Running:\nregsvr32 {0}\n", argument);
+            registerProfilerDll.WaitForExit();
 
             var startInfo = new ProcessStartInfo(Path.Combine(currentDirectory, "ProfilerTarget.exe"));
-            //if (startInfo.EnvironmentVariables.ContainsKey("Cor_Profiler"))
-            //    startInfo.EnvironmentVariables.Remove("Cor_Profiler");
-           
-            //if (startInfo.EnvironmentVariables.ContainsKey("Cor_Enable_Profiling"))
-            //    startInfo.EnvironmentVariables.Remove("Cor_Enable_Profiling");
+            if (startInfo.EnvironmentVariables.ContainsKey("Cor_Profiler"))
+                startInfo.EnvironmentVariables.Remove("Cor_Profiler");
 
-            //startInfo.EnvironmentVariables.Add("Cor_Profiler", "{BDD57A0C-D4F7-486D-A8CA-86070DC12FA0}");
-            //startInfo.EnvironmentVariables.Add("Cor_Enable_Profiling", "1");
+            if (startInfo.EnvironmentVariables.ContainsKey("Cor_Enable_Profiling"))
+                startInfo.EnvironmentVariables.Remove("Cor_Enable_Profiling");
+
+            startInfo.EnvironmentVariables.Add("Cor_Profiler", "{BDD57A0C-D4F7-486D-A8CA-86070DC12FA0}");
+            startInfo.EnvironmentVariables.Add("Cor_Enable_Profiling", "1");
             startInfo.UseShellExecute = false;
 
             Console.WriteLine("Launching:\n{0}\n", startInfo.FileName);
